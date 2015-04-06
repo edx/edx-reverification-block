621cc3024963c6ecac423c68b119ffde9c2b019d
aamir-khan <aamir@edx.org>
2015-04-06 16:15:12 +0500

    integrated the re-verification service

--- edx_reverification_block/xblock/reverification_block.py
+++ edx_reverification_block/xblock/reverification_block.py
@@ -30,27 +30,25 @@ class ReverificationBlock(XBlock):
         course_id = self.get_course_id()
         item_id = unicode(self.scope_ids.usage_id)
         checkpoint_name = CHECKPOINT_NAME
-        # TODO: How to get the user object from runtime to check the status of that user in the given checkpoint?
+        user_id = unicode(self.scope_ids.user_id)
         verification_status = self.runtime.service(self, "reverification").get_status(
+            user_id=user_id,
             course_id=course_id,
             checkpoint_name=checkpoint_name
         )
-        if verification_status:
+        if verification_status and 0:
             # TODO: What message will be displayed to user if it is already has any status?
-            frag = Fragment(unicode(verification_status))
+            frag = Fragment(unicode(reverification_link))
             return frag
-        self.runtime.service(self, "reverification").start_verification(
+        reverification_link = self.runtime.service(self, "reverification").start_verification(
             course_id=course_id,
             checkpoint_name=checkpoint_name,
             item_id=item_id
         )
-        # TODO: How to get the org from the xmodule_runtime??
-        org = u"MIT"
+        org = self.get_org()
         html_str = pkg_resources.resource_string(__name__, "static/html/reverification.html")
         frag = Fragment(unicode(html_str).format(
-            course_id=course_id,
-            checkpoint_name=checkpoint_name,
-            item_id=item_id,
+            reverification_link=reverification_link,
             org=org
         ))
         return frag
@@ -70,3 +68,11 @@ class ReverificationBlock(XBlock):
         # This is not the real way course_ids should work, but this is a
         # temporary expediency for LMS integration
         return self.course_id if hasattr(self, "xmodule_runtime") else "edX/Enchantment_101/April_1"
+
+    def get_org(self):
+        """ Return the org
+
+        """
+        # This is not the real way getting the org should work, but this is a
+        # temporary expediency for LMS integration
+        return self.xmodule_runtime.course_id.org if hasattr((self, "xmodule_runtime")) else "edX ORG"
