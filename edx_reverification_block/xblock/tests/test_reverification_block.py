68800ef434b64f6a5319b15c02d3d537f461e4d5
zubair-arbi <zubair.afzal@arbisoft.com>
2015-04-17 16:52:45 +0500

    add basic tests of edx-reverification-xblock ECOM-1434

---
+++ edx_reverification_block/xblock/tests/test_reverification_block.py
@@ -0,0 +1,56 @@
+"""
+Tests the edX Reverification XBlock functionality.
+"""
+import json
+import os
+from mock import Mock
+
+from django.test import TestCase
+
+from workbench.test_utils import XBlockHandlerTestCaseMixin, scenario
+
+
+TESTS_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class TestReverificationBlock(XBlockHandlerTestCaseMixin, TestCase):
+
+    @scenario(TESTS_BASE_DIR + '/data/basic_scenario.xml')
+    def test_load_student_view(self, xblock):
+        """
+        Reverification XBlock basic test for verifying that the user gets some
+        HTML.
+        """
+        # Test that creating Reverification XBlock for the first time user gets
+        # message to configure it
+        xblock_fragment = self.runtime.render(xblock, "student_view")
+        self.assertTrue('edx-reverification-block' in xblock_fragment.body_html())
+        self.assertTrue('You need to configure the xBlock before publishing it' in xblock_fragment.body_html())
+
+        # Now set 'related_assessment' and 'attempts' fields and check that
+        # values of these fields appear in rendered response and also test
+        # that configuration message does not appear
+        data = json.dumps({'related_assessment': 'FinalExam', 'attempts': 5})
+        resp = self.request(xblock, 'studio_submit', data, response_format='json')
+        self.assertTrue(resp.get('result'))
+        xblock_fragment = self.runtime.render(xblock, "student_view")
+        self.assertTrue('FinalExam' in xblock_fragment.body_html())
+        self.assertFalse('You need to configure the xBlock before publishing it' in xblock_fragment.body_html())
+
+        # Configure a dummy "ReverificationService" with dummy responses
+        DummyReverificationService = Mock()
+        dummy_link = '/reverify/COURSE_ID/CHECKPOINT_NAME/COURSEWARE_LOCATION'
+        attrs = {'get_status.return_value': None, 'start_verification.return_value': dummy_link}
+        DummyReverificationService.configure_mock(**attrs)
+        self.runtime._services['reverification'] = DummyReverificationService
+
+        # Case #1: 'VerificationStatus' of user is None
+        # Test that verification link is present in response
+        xblock_fragment = self.runtime.render(xblock, "student_view")
+        self.assertTrue(dummy_link in xblock_fragment.body_html())
+
+        # Case #2: 'VerificationStatus' of user is 'submitted'
+        # Test that verification status is present in response
+        DummyReverificationService.get_status.return_value = "submitted"
+        xblock_fragment = self.runtime.render(xblock, "student_view")
+        self.assertTrue('submitted' in xblock_fragment.body_html())
