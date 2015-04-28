82138b5771fd432a5a981e5ff75fa86b34397161
Will Daly <will@edx.org>
2015-04-28 08:33:00 -0400

    Add additional message states
    Add message styling and copy for:
    * Submitted verification
    * Verification approved
    * Verification denied
    * Verification error

--- edx_reverification_block/xblock/tests/test_reverification_block.py
+++ edx_reverification_block/xblock/tests/test_reverification_block.py
@@ -50,9 +50,7 @@ class TestStudentView(XBlockHandlerTestCaseMixin, TestCase):
 
     @scenario(TESTS_BASE_DIR + '/data/basic_scenario.xml', user_id='bob')
     def test_student_view_ready_to_reverify(self, xblock):
-        xblock_fragment = self.runtime.render(xblock, "student_view")
-        html = xblock_fragment.body_html()
-        self.assertIn('Re-Verify Now', html)
+        self._assert_in_student_view(xblock, "Re-Verify Now")
 
         reverify_url = reverse('stub_reverify_flow', args=(
             xblock.course_id,
@@ -60,11 +58,18 @@ class TestStudentView(XBlockHandlerTestCaseMixin, TestCase):
             xblock.related_assessment,
             xblock.scope_ids.user_id),
         )
-        self.assertIn(reverify_url, html)
+        self._assert_in_student_view(xblock, reverify_url)
 
-    @ddt.data("submitted", "approved", "denied", "error")
+    @ddt.data(
+        ("submitted", "submitted"),
+        ("approved", "approved"),
+        ("denied", "not successful"),
+        ("error", "error"),
+        ("unexpected", "error"),
+    )
+    @ddt.unpack
     @scenario(TESTS_BASE_DIR + '/data/basic_scenario.xml', user_id='bob')
-    def test_student_view_reverification_status(self, xblock, status):
+    def test_student_view_reverification_status(self, xblock, status, expected_content):
         # Simulate the verification status
         VerificationStatus.objects.create(
             course_id=xblock.course_id,
@@ -74,8 +79,7 @@ class TestStudentView(XBlockHandlerTestCaseMixin, TestCase):
         )
 
         # Check that the status is displayed correctly
-        xblock_fragment = self.runtime.render(xblock, "student_view")
-        self.assertIn(status, xblock_fragment.body_html())
+        self._assert_in_student_view(xblock, expected_content)
 
     @scenario(TESTS_BASE_DIR + '/data/basic_scenario.xml', user_id='bob')
     def test_skip_reverification(self, xblock):
@@ -84,5 +88,23 @@ class TestStudentView(XBlockHandlerTestCaseMixin, TestCase):
         self.assertTrue(response['success'])
 
         # Reloading the student view, we should see that we've skipped
+        self._assert_in_student_view(xblock, "skipped")
+
+    @scenario(TESTS_BASE_DIR + '/data/basic_scenario.xml', user_id='bob')
+    def test_render_support_email(self, xblock):
+        # Simulate an error verification status
+        VerificationStatus.objects.create(
+            course_id=xblock.course_id,
+            checkpoint_name=xblock.related_assessment,
+            user_id=xblock.scope_ids.user_id,
+            status="error"
+        )
+
+        # Check that the support email is displayed correctly
+        expected_content = '<a href="mailto:support@edx.org">support@edx.org</a>'
+        self._assert_in_student_view(xblock, expected_content)
+
+    def _assert_in_student_view(self, xblock, expected_content):
+        """Check that the student view contains the expected content. """
         xblock_fragment = self.runtime.render(xblock, "student_view")
-        self.assertIn("skipped", xblock_fragment.body_html())
+        self.assertIn(expected_content, xblock_fragment.body_html())
