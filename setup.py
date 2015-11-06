cd9a252e217209e960e1cf9ac9bb7293e8085ed1
muhammad-ammar <mammar@gmail.com>
2015-11-06 12:08:54 +0500

    bump library version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edx-reverification-block',
-    version='1.0',
+    version='0.0.5',
     author='edX',
     url='http://github.com/edx/edx-reverification-block',
     description='An XBlock for in-course reverification.',
